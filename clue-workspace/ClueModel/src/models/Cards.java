c656392bcb335eb3cd2a2e00ef35979f34f2bce7
pameirelles <rafaelpmeirelles@gmail.com>
2022-10-13 22:50:49 -0300

    Apagado comentario singleton antigo

--- clue-workspace/ClueModel/src/models/Cards.java
+++ clue-workspace/ClueModel/src/models/Cards.java
@@ -9,21 +9,5 @@ public class Cards implements CardFactory {
 
 	@Override
 	public void setName(String name) { this.name = name; }
-	
-//	private static volatile Cards instance = null;
-//	
-//	private Cards() {}
-//	
-//	public static Cards getInstance() {
-//		if (instance == null) {
-//			
-////			making thread safe
-//			synchronized (Cards.class) {
-////				check again as multiple threads can reach above step
-//				if (instance == null)
-//					instance = new Cards();
-//			}
-//		}
-//		return instance;
-//	}
+
 }
--- clue-workspace/ClueModel/src/models/Players.java
+++ clue-workspace/ClueModel/src/models/Players.java
@@ -27,21 +27,4 @@ public class Players implements PlayerFactory {
 		
 		cardsArr = newArray;
 	}
-
-//	private static volatile Players instance = null;
-//	
-//	private Players() {}
-//	
-//	public static Players getInstance() {
-//		if (instance == null) {
-//			
-////			making thread safe
-//			synchronized (Players.class) {
-////				check again as multiple threads can reach above step
-//				if (instance == null)
-//					instance = new Players();
-//			}
-//		}
-//		return instance;
-//	}
 }
