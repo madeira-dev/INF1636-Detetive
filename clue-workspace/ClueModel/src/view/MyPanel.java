8d479672ee7f7bc3e695d972f4875bd64dfc7cf3
thiagoluisbecker <thiagobeckerrocha@gmail.com>
2022-10-27 23:55:33 -0300

    interface grafica menuInicial mas tem q passar o mouse em cima dos botoes p funfar (corrigir)

---
+++ clue-workspace/ClueModel/src/view/MyPanel.java
@@ -0,0 +1,16 @@
+package view;
+import javax.swing.*;
+import java.awt.*;
+
+public class MyPanel extends JPanel {
+    public Image img;
+
+    MyPanel(Image image){
+        img=image;
+    }
+
+    public void paintComponent(Graphics g2d){
+        super.paintComponent(g2d);
+        g2d.drawImage(img, 0, 0, null);
+    }
+}
