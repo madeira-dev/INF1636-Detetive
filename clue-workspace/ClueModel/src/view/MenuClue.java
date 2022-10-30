9572e19d905039b5f76d26e7ecafce2829ef0b55
thiagoluisbecker <thiagobeckerrocha@gmail.com>
2022-10-30 14:43:49 -0300

    seleção de personagens feita

---
+++ clue-workspace/ClueModel/src/view/MenuClue.java
@@ -0,0 +1,50 @@
+package view;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
+
+
+    public class MenuClue extends JFrame {
+
+        Image img_menu;
+        JPanel p;
+        JButton continuar= new JButton("Continuar");
+        JButton nova_partida = new JButton("Nova Partida");
+
+
+        public MenuClue()
+        {
+            try
+            {
+                img_menu = ImageIO.read(new File("E:\\Users\\thiag\\IdeaProjects\\INF1636-Detetive\\clue-workspace\\ClueModel\\src\\view\\Clue1.jpg"));
+            }
+            catch(IOException e)
+            {
+                System.out.println(e.getMessage());
+            }
+            p = new MyPanel(img_menu);
+            continuar.setBounds(700,300,150,40);
+            nova_partida.setBounds(700,370,150,40);
+            this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            this.setLayout(null);
+            this.setSize(966, 700);
+            this.setVisible(true);
+            this.setTitle("Menu Inicial");
+            this.add(continuar);
+            this.add(nova_partida);
+
+        }
+
+        public void paint(Graphics g2d){
+            super.paint(g2d);
+            g2d.drawImage(img_menu,0,0,this);
+
+        }
+
+    }
+
+
---
+++ clue-workspace/ClueModel/src/view/SelecaoPersonagem.java
@@ -0,0 +1,72 @@
+package view;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
+
+
+public class SelecaoPersonagem extends JFrame {
+
+    Image img_menu;
+    JPanel p;
+    JCheckBox scarlett= new JCheckBox("Mrs. Scarlett");
+    JCheckBox mustard= new JCheckBox("Coronel Mustard");
+    JCheckBox white= new JCheckBox("Mrs. White");
+    JCheckBox peacock= new JCheckBox("Mrs. Peacock");
+    JCheckBox plum= new JCheckBox("Prof. Plum");
+    JCheckBox green= new JCheckBox("Rev. Green");
+
+
+    JButton jogar = new JButton("Jogar");
+
+
+    public SelecaoPersonagem()
+    {
+        try
+        {
+            img_menu = ImageIO.read(new File("E:\\Users\\thiag\\IdeaProjects\\INF1636-Detetive\\clue-workspace\\ClueModel\\src\\view\\CluePersonagens.png"));
+        }
+        catch(IOException e)
+        {
+            System.out.println(e.getMessage());
+        }
+        p = new MyPanel(img_menu);
+        jogar.setBounds(600,350,150,40);
+        scarlett.setBounds(600,100,150,40);
+        green.setBounds(600,140,150,40);
+        white.setBounds(600,180,150,40);
+        mustard.setBounds(600,220,150,40);
+        plum.setBounds(600,260,150,40);
+        peacock.setBounds(600,300,150,40);
+
+        //nova_partida.setBounds(700,370,150,40);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(null);
+        this.setSize(966, 700);
+        this.setVisible(true);
+        this.setTitle("Menu Inicial");
+
+        this.add(jogar);
+        this.add(scarlett);
+        this.add(green);
+        this.add(white);
+        this.add(mustard);
+        this.add(plum);
+        this.add(peacock);
+
+        //this.add(nova_partida);
+
+    }
+
+    public void paint(Graphics g2d){
+        super.paint(g2d);
+        g2d.drawImage(img_menu,0,0,this);
+
+    }
+
+}
+
+
