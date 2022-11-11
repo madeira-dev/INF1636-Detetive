a76d0e6803945a902ea8a5060d3bed11da728ceb
pameirelles <rafaelpmeirelles@gmail.com>
2022-11-11 11:45:35 -0300

    Turn in board

--- clue-workspace/ClueModel/src/view/Main.java
+++ clue-workspace/ClueModel/src/view/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		JogoClue tela_jogo = new JogoClue();
 		// SelecaoPersonagem tela_personagem = new SelecaoPersonagem();
-		// MenuClue tela_menu = new MenuClue();
+		 MenuClue tela_menu = new MenuClue();
 
 		// PlayerCards cards = new PlayerCards();
 		// Notepad tela_notepad = new Notepad();
