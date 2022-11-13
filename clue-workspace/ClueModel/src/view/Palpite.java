dbe2100f4acc4258e136bbc98397b66e41e65b44
pameirelles <rafaelpmeirelles@gmail.com>
2022-11-13 13:42:45 -0300

    Separação de colunas na tela de palpite

--- clue-workspace/ClueModel/src/view/Palpite.java
+++ clue-workspace/ClueModel/src/view/Palpite.java
@@ -17,6 +17,7 @@ public class Palpite  extends JFrame implements ActionListener {
     ButtonGroup armas_group;
     ButtonGroup personagens_group;
     ButtonGroup comodos_group;
+    JPanel[] panels;
 
     public Palpite(){
 
@@ -28,25 +29,33 @@ public class Palpite  extends JFrame implements ActionListener {
         personagens_group = new ButtonGroup();
         comodos_group = new ButtonGroup();
 
+        panels = new JPanel[3];
+
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         this.setSize(600, 400);
         this.setVisible(true);
         this.setTitle("Tela de Palpite");
-        this.setLayout(new GridLayout(Componentes.num_comodos(), 3));
+        this.setLayout(new GridLayout(1, 3));
+
+        for(int i=0; i< 3; i++){
+            panels[i] = new JPanel();
+            panels[i].setLayout(new BoxLayout(panels[i], BoxLayout.PAGE_AXIS));
+            this.add(panels[i]);
+        }
 
         for(int i=0; i < Componentes.num_armas(); i++){
             armas[i] = new JRadioButton(Componentes.armas_cartas()[i].getName());
-            this.add(armas[i]);
+            this.panels[0].add(armas[i]);
             armas_group.add(armas[i]);
         }
         for(int i=0; i < Componentes.num_personagens(); i++){
             personagens[i] = new JRadioButton(Componentes.personagens_cartas()[i].getName());
-            this.add(personagens[i]);
+            this.panels[1].add(personagens[i]);
             personagens_group.add(personagens[i]);
         }
         for(int i=0; i < Componentes.num_comodos(); i++){
             comodos[i] = new JRadioButton(Componentes.comodos_cartas()[i].getName());
-            this.add(comodos[i]);
+            this.panels[2].add(comodos[i]);
             comodos_group.add(comodos[i]);
         }
     }
