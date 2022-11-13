367271d7acd84cd0766a8af5b1157cd297288416
Gabriel Madeira <gabrielmadeira2002@gmail.com>
2022-11-13 13:50:03 -0300

    notepad implmented

--- clue-workspace/ClueModel/src/view/Main.java
+++ clue-workspace/ClueModel/src/view/Main.java
@@ -1,12 +1,13 @@
 package view;
 
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) {	
+		
 		JogoClue tela_jogo = new JogoClue();
 //		SelecaoPersonagem tela_personagem = new SelecaoPersonagem();
 //		MenuClue tela_menu = new MenuClue();
 
 		// PlayerCards cards = new PlayerCards();
-//		 Notepad tela_notepad = new Notepad();
+//		 Notepad tela_notepad = new Notepad(new boolean[6], new boolean[6], new boolean[9]);
 	}
 }
