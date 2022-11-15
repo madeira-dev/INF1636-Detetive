581949d743b80ccbd0c55682ffe014257079a6b6
Gabriel Madeira <gabrielmadeira2002@gmail.com>
2022-11-15 18:46:53 -0300

    updates suspects names in componentes

--- clue-workspace/ClueModel/src/models/Componentes.java
+++ clue-workspace/ClueModel/src/models/Componentes.java
@@ -2,28 +2,32 @@ package models;
 import java.util.Random;
 
 public final class Componentes {
+	
 	public static int num_personagens(){return 6;}
+	
 	public static int num_armas(){return 6;}
+	
 	public static int num_comodos(){return 9;}
-	public static Card[] personagens_cartas(){
+	
+	public static Card[] personagens_cartas() {
 	Card[] personagens = {
-      new Card("Srta. Scarlett", "personagem"),
-      new Card("Reverendo Green", "personagem"),
-      new Card("Srta. Peacock", "personagem"),
-      new Card("Professor Plum", "personagem"),
-      new Card("Coronel Mustard", "personagem"),
-      new Card("Srta. White", "personagem")
+      new Card("Scarlet", "personagem"),
+      new Card("Green", "personagem"),
+      new Card("Peacock", "personagem"),
+      new Card("Plum", "personagem"),
+      new Card("Mustard", "personagem"),
+      new Card("White", "personagem")
       };
     	return personagens;
 	}
-	
+
 	public static Card[] armas_cartas() {
 		Card[] armas_cartas = {
 				new Card("Corda", "arma"),
-				new Card("Cano de Chumbo", "arma"),
+				new Card("Cano", "arma"),
 				new Card("Faca", "arma"),
-				new Card("Chave Inglesa", "arma"),
-				new Card("Castiçal", "arma"),
+				new Card("ChaveInglesa", "arma"),
+				new Card("Castical", "arma"),
 				new Card("Revolver", "arma")
 				};
 	
@@ -32,15 +36,15 @@ public final class Componentes {
 	
 	public static Card[] comodos_cartas() {
 		Card[] comodos_cartas = {
-				new Card("Billard Room", "comodo"),
-				new Card("Library", "comodo"),
-				new Card("Study", "comodo"),
-				new Card("Hall", "comodo"),
-				new Card("Lounge", "comodo"),
-				new Card("Dining Room", "comodo"),
-				new Card("Kitchen", "comodo"),
-				new Card("Ball Room", "comodo"),
-				new Card("Conservatory", "comodo")
+				new Card("Biblioteca", "comodo"),
+				new Card("Cozinha", "comodo"),
+				new Card("Entrada", "comodo"),
+				new Card("Escritorio", "comodo"),
+				new Card("JardimInverno", "comodo"),
+				new Card("SalaDeEstar", "comodo"),
+				new Card("SalaDeJantar", "comodo"),
+				new Card("SalaDeMusica", "comodo"),
+				new Card("SalaoDeJogos", "comodo")
 				};
 		return comodos_cartas;
 		}
