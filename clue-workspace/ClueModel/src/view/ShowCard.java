a07755b7a9f3d6ec74f663b747cb59658e7a576f
pameirelles <rafaelpmeirelles@gmail.com>
2022-11-15 18:41:19 -0300

    Show card

--- clue-workspace/ClueModel/src/view/ShowCard.java
+++ clue-workspace/ClueModel/src/view/ShowCard.java
@@ -12,19 +12,24 @@ import java.io.IOException;
 
 public class ShowCard extends JFrame {
     JLabel texto;
-    Card card;
+    Image imagem;
+
 
     ShowCard(Card carta, Player mostrador) throws IOException {
-        card = carta;
         texto = new JLabel(String.format("%s te mostrou %s", mostrador.getName(), carta.getName()), SwingConstants.CENTER);
         texto.setBounds(0, 0, 400, 50);
-        Image img = ImageIO.read(new File(String.format("Imagens/%s/%s")))
+        imagem = ImageIO.read(new File(String.format("Imagens/%s/%s", carta.get_folder(), carta.getName())));
+
         this.add(texto);
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         this.setSize(500, 400);
         this.setVisible(true);
         this.setTitle("Receber Carta");
         this.setLayout(null);
+    }
+    public void paint(Graphics g2d) {
+        super.paint(g2d);
+        g2d.drawImage(imagem,220,100,this);
     }
 
 }
