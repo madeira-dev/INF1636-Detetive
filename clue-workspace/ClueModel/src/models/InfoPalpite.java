fdc7941d836e2def1dae14e0ee03ff104405cf54
pameirelles <rafaelpmeirelles@gmail.com>
2022-11-15 18:02:16 -0300

    InfoPalpite

---
+++ clue-workspace/ClueModel/src/models/InfoPalpite.java
@@ -0,0 +1,19 @@
+package models;
+
+public class InfoPalpite {
+    private final Player player;
+    private final Card[] cards;
+
+    public InfoPalpite(Player player, Card[] cards){
+        this.player = player;
+        this.cards = cards;
+    }
+
+    public Card[] getCards() {
+        return cards;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+}
