1e68ae24edbdeb22e9644d4bed84adefc8bafa3d
Gabriel Madeira <gabrielmadeira2002@gmail.com>
2022-11-27 12:03:39 -0300

    codigos formatados e janelas fechando

--- clue-workspace/ClueModel/src/view/SelecaoPersonagem.java
+++ clue-workspace/ClueModel/src/view/SelecaoPersonagem.java
@@ -12,104 +12,96 @@ import java.awt.event.*;
 import java.io.File;
 import java.io.IOException;
 
+public class SelecaoPersonagem extends JFrame implements ActionListener {
 
+	Image img_menu;
+	JPanel p;
 
-public class SelecaoPersonagem extends JFrame implements ActionListener{
+	JCheckBox scarlett = new JCheckBox("Mrs. Scarlett");
+	JCheckBox mustard = new JCheckBox("Coronel Mustard");
+	JCheckBox white = new JCheckBox("Mrs. White");
+	JCheckBox peacock = new JCheckBox("Mrs. Peacock");
+	JCheckBox plum = new JCheckBox("Prof. Plum");
+	JCheckBox green = new JCheckBox("Rev. Green");
 
-    Image img_menu;
-    JPanel p;
-    
-    JCheckBox scarlett= new JCheckBox("Mrs. Scarlett");
-    JCheckBox mustard= new JCheckBox("Coronel Mustard");
-    JCheckBox white= new JCheckBox("Mrs. White");
-    JCheckBox peacock= new JCheckBox("Mrs. Peacock");
-    JCheckBox plum= new JCheckBox("Prof. Plum");
-    JCheckBox green= new JCheckBox("Rev. Green");
+	JButton jogar = new JButton("Jogar");
 
-
-    JButton jogar = new JButton("Jogar");
-
-
-    public SelecaoPersonagem() {
-        try {
-            img_menu = ImageIO.read(new File("imagens/Tabuleiros/CluePersonagens.png"));
-        }
-        catch(IOException e) {
-            System.out.println(e.getMessage());
-        }
-        
-        p = new MyPanel(img_menu);
-        jogar.setBounds(600,440,150,40);
-        jogar.addActionListener(this);
-        scarlett.setBounds(600,190,150,40);
-        green.setBounds(600,230,150,40);
-        white.setBounds(600,270,150,40);
-        mustard.setBounds(600,310,150,40);
-        plum.setBounds(600,350,150,40);
-        peacock.setBounds(600,390,150,40);
-
-        //nova_partida.setBounds(700,370,150,40);
-        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        this.setLayout(null);
-        this.setSize(966, 700);
-        this.setVisible(true);
-        this.setTitle("Seleção Personagem");
-
-        this.add(jogar);
-        this.add(scarlett);
-        this.add(green);
-        this.add(white);
-        this.add(mustard);
-        this.add(plum);
-        this.add(peacock);
-    }
-    
-    public void actionPerformed(ActionEvent e) {
-    	Player []lista_jogadores = new Player[6];
-    	if(e.getSource() == jogar) {
-    		this.dispose();
-    		
-    		lista_jogadores = produzJogadores();
-
-    	}
-    }
-   
-    public Player[] produzJogadores() {
-    	int num_jogadores = 0 ;
-    	Player lista_jogadores[] = new Player[6];
-    	
-    	if(scarlett.isSelected()) {
-    		Controller.add_player("Srta. Scarlett");
+	public SelecaoPersonagem() {
+		try {
+			img_menu = ImageIO.read(new File("imagens/Tabuleiros/CluePersonagens.png"));
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
 		}
-		if(green.isSelected()) {
-            Controller.add_player("Reverendo Green");
+
+		p = new MyPanel(img_menu);
+		jogar.setBounds(600, 440, 150, 40);
+		jogar.addActionListener(this);
+		scarlett.setBounds(600, 190, 150, 40);
+		green.setBounds(600, 230, 150, 40);
+		white.setBounds(600, 270, 150, 40);
+		mustard.setBounds(600, 310, 150, 40);
+		plum.setBounds(600, 350, 150, 40);
+		peacock.setBounds(600, 390, 150, 40);
+
+		// nova_partida.setBounds(700,370,150,40);
+		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		this.setLayout(null);
+		this.setSize(966, 700);
+		this.setVisible(true);
+		this.setTitle("Seleção Personagem");
+
+		this.add(jogar);
+		this.add(scarlett);
+		this.add(green);
+		this.add(white);
+		this.add(mustard);
+		this.add(plum);
+		this.add(peacock);
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		Player[] lista_jogadores = new Player[6];
+		if (e.getSource() == jogar) {
+			this.dispose();
+			lista_jogadores = produzJogadores();
+
 		}
-		if(white.isSelected()) {
-            Controller.add_player("Srta. White");
+	}
+
+	public Player[] produzJogadores() {
+		int num_jogadores = 0;
+		Player lista_jogadores[] = new Player[6];
+
+		if (scarlett.isSelected()) {
+			Controller.add_player("Srta. Scarlett");
 		}
-		if(mustard.isSelected()) {
-            Controller.add_player("Coronel Mustard");
+		if (green.isSelected()) {
+			Controller.add_player("Reverendo Green");
 		}
-		if(plum.isSelected()) {
-            Controller.add_player("Professor Plum");
+		if (white.isSelected()) {
+			Controller.add_player("Srta. White");
 		}
-		if(peacock.isSelected()) {
-            Controller.add_player("Srta. Peacock");
+		if (mustard.isSelected()) {
+			Controller.add_player("Coronel Mustard");
 		}
-		if(Controller.get_num_players() >= 3) {
+		if (plum.isSelected()) {
+			Controller.add_player("Professor Plum");
+		}
+		if (peacock.isSelected()) {
+			Controller.add_player("Srta. Peacock");
+		}
+		if (Controller.get_num_players() >= 3) {
 			JogoClue tela_jogo = new JogoClue();
-            Controller.init_all();
+			Controller.init_all();
 		}
 
 		return lista_jogadores;
-    }
-   
-    public void paint(Graphics g2d) {
-        super.paint(g2d);
-        g2d.drawImage(img_menu,0,55,this);
+	}
 
-    }
+	public void paint(Graphics g2d) {
+		super.paint(g2d);
+		g2d.drawImage(img_menu, 0, 55, this);
+
+	}
 
 }
-
-
--- clue-workspace/ClueModel/src/view/ShowCard.java
+++ clue-workspace/ClueModel/src/view/ShowCard.java
@@ -11,27 +11,30 @@ import java.io.File;
 import java.io.IOException;
 
 public class ShowCard extends JFrame {
-    JLabel texto;
-    Image imagem;
+	JLabel texto;
+	Image imagem;
 
+	ShowCard(Card carta, Player mostrador) throws IOException {
+		this.setLayout(new BorderLayout());
+		texto = new JLabel(String.format("%s te mostrou %s", mostrador.getCharacter(), carta.getName()),
+				SwingConstants.CENTER);
+		texto.setBounds(0, 0, 400, 50);
+		// imagem = ImageIO.read(new
+		// File(String.format("clue-workspace/ClueModel/imagens/Armas/Cano.jpg",
+		// carta.get_folder(), carta.getName())));
+		imagem = ImageIO.read(new File(String.format("imagens/%s/%s.jpg", carta.get_folder(), carta.getName())));
 
-    ShowCard(Card carta, Player mostrador) throws IOException {
-        this.setLayout(new BorderLayout());
-        texto = new JLabel(String.format("%s te mostrou %s", mostrador.getCharacter(), carta.getName()), SwingConstants.CENTER);
-        texto.setBounds(0, 0, 400, 50);
-        //imagem = ImageIO.read(new File(String.format("clue-workspace/ClueModel/imagens/Armas/Cano.jpg", carta.get_folder(), carta.getName())));
-        imagem = ImageIO.read(new File(String.format("imagens/%s/%s.jpg", carta.get_folder(), carta.getName())));
+		this.add(texto, BorderLayout.PAGE_START);
+		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		this.setSize(500, 400);
+		this.setVisible(true);
+		this.setTitle("Receber Carta");
 
-        this.add(texto, BorderLayout.PAGE_START);
-        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        this.setSize(500, 400);
-        this.setVisible(true);
-        this.setTitle("Receber Carta");
+	}
 
-    }
-    public void paint(Graphics g2d) {
-        super.paint(g2d);
-        g2d.drawImage(imagem,170,100,this);
-    }
+	public void paint(Graphics g2d) {
+		super.paint(g2d);
+		g2d.drawImage(imagem, 170, 100, this);
+	}
 
 }
